96b10019f42345f8fab6cac5da0c0246d61d86f5
priyankakanse123 <priyankakanse93@gmail.com>
2016-02-16 09:59:47 +0530

    first commit

---
+++ demoMerge/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  demoMerge
+//
+//  Created by BridgeLabz on 16/02/16.
+//  Copyright © 2016 BridgeLabz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        print ("hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
